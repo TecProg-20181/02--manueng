b021ed6685f001a43eac2941d5de8b89b4206984
Emanuel <manusenac@hotmail.com>
2018-04-11 21:57:51 -0300

    folha de estilo identacao

--- hang.py
+++ hang.py
@@ -16,26 +16,26 @@ class word(object):
          self.secretword=secretword
      
      def guessedword(self,game): 
-      letter=game.letter 
-      guessed=''
-      lettersGuessed=game.lettersGuessed
-      for letter in self.secretword:
-       if letter in lettersGuessed:
-         guessed += letter      
-       else:
-         guessed += '_ ' 
+         letter=game.letter 
+         guessed=''
+         lettersGuessed=game.lettersGuessed
+         for letter in self.secretword:
+          if letter in lettersGuessed:
+             guessed += letter      
+          else:
+             guessed += '_ ' 
       
-      return guessed
+         return guessed
 
      def isWordGuessed(self, game):
-        lettersGuessed=game.lettersGuessed
-        for letter in self.getsecretword():
-         if letter in lettersGuessed:
-            pass
-         else:
-            return False
+         lettersGuessed=game.lettersGuessed
+         for letter in self.getsecretword():
+          if letter in lettersGuessed:
+             pass
+          else:
+             return False
 
-        return True
+         return True
 
      def quantitydifferent(self):
          secretword=self.getsecretword()
@@ -79,42 +79,42 @@ class game(object):
     letter='y'
      
     def __init__(self):
-        pass 
+         pass 
     def loadWords(self):
-     """
-     Depending on the size of the word list, this function may
-     take a while to finish.
-     """
-     print "Loading word list from file..."
-     # inFile: file
-     inFile = open(WORDLIST_FILENAME, 'r', 0)
-     # line: string
-     line = inFile.readline()
-     # wordlist: list of strings
-     wordlist = string.split(line)
-     print "  ", len(wordlist), "words loaded."
-     return random.choice(wordlist)
+         """
+         Depending on the size of the word list, this function may
+         take a while to finish.
+         """
+         print "Loading word list from file..."
+         # inFile: file
+         inFile = open(WORDLIST_FILENAME, 'r', 0)
+         # line: string
+         line = inFile.readline()
+         # wordlist: list of strings
+         wordlist = string.split(line)
+         print "  ", len(wordlist), "words loaded."
+         return random.choice(wordlist)
     def gametry(self,secretWord) :
     
-      print 'Welcome to the game, Hangam!'
-      print 'I am thinking of a word that is', len(secretWord), ' letters long.'
-      print '-------------'
-      worda= word(secretWord)
-      worda.quantitydifferent()
-      guessed=' '    
-      while worda.isWordGuessed(self) == False and self.guesses >0:
-         print 'You have ', self.guesses, 'guesses left.'
-         self.lettersavailable()
-         letter = raw_input('Please guess a letter: ')
-         self.setletter(letter) 
-         result=self.triedletter(guessed)     
-         if result :
+         print 'Welcome to the game, Hangam!'
+         print 'I am thinking of a word that is', len(secretWord), ' letters long.'
+         print '-------------'
+         worda= word(secretWord)
+         worda.quantitydifferent()
+         guessed=' '    
+         while worda.isWordGuessed(self) == False and self.getguesses() >0:
+           print 'You have ', self.guesses, 'guesses left.'
+           self.lettersavailable()
+           letter = raw_input('Please guess a letter: ')
+           self.setletter(letter) 
+           result=self.triedletter(guessed)     
+           if result :
             guessed=self.score(worda)    
-      else:
+         else:
            if worda.isWordGuessed(self) == True:
-             print 'Congratulations, you won!'
+            print 'Congratulations, you won!'
            else:
-             print 'Sorry, you ran out of guesses. The word was ', worda.getsecretword(), '.'
+            print 'Sorry, you ran out of guesses. The word was ', worda.getsecretword(), '.'
     def getAvailableLetters(self):
         import string
         # 'abcdefghijklmnopqrstuvwxyz'
@@ -122,35 +122,42 @@ class game(object):
 
         return available
     def lettersavailable(self):
-         available = self.getAvailableLetters()
-         for letter in available:
-          if letter in self.getletterguessed():
-                available = available.replace(letter, '')
-         print 'Available letters', available 
+        available = self.getAvailableLetters()
+        for letter in available:
+            if letter in self.getletterguessed():
+               available = available.replace(letter, '')
+        print 'Available letters', available 
     def  triedletter(self,guessed):
-         letter=self.getletter()     
-         if letter in self.getletterguessed():
-               print 'Oops! You have already guessed that letter: ', guessed
-               return False
-         else: 
-            self.lettersGuessed.append(letter) 
-         return True 
+        letter=self.getletter()     
+        if letter in self.getletterguessed():
+          print 'Oops! You have already guessed that letter: ', guessed
+          return False
+        else: 
+          self.setlettersguessed(letter) 
+        return True 
     def score(self,worda):
-         letter=self.letter          
-         guessed=worda.guessedword(self)          
-         if self.getletter() in worda.getsecretword():
-            print 'Good Guess: ', guessed
-         else:
-            self.guesses -=1 
-            print 'Oops! That letter is not in my word: ',  guessed
-         print '------------'
-         return guessed
+        letter=self.letter          
+        guessed=worda.guessedword(self)          
+        if self.getletter() in worda.getsecretword():
+          print 'Good Guess: ', guessed
+        else:
+          guesses=self.getguesses()-1
+          self.setguesses(guesses)
+          print 'Oops! That letter is not in my word: ',  guessed
+        print '------------'
+        return guessed
     def setletter(self,letter):
-           self.letter=letter 
+        self.letter=letter 
     def getletter(self):
-           return self.letter
+        return self.letter
     def getletterguessed(self):
-            return self.lettersGuessed
+        return self.lettersGuessed
+    def setlettersguessed(self,letter):
+        self.lettersGuessed.append(letter)
+    def setguesses(self,guesses):
+        self.guesses=guesses  
+    def getguesses  (self):
+        return self.guesses  
 
       
 game1=game()
