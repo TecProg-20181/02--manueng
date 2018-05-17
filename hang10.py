213ed162603be510804cf2793714e1814131b7c3
emanueng <emanuelhbga@gmail.com>
2018-05-17 20:02:28 -0300

    trabalho quase finalizado

--- hang10.py
+++ hang10.py
@@ -10,12 +10,32 @@ WORDLIST_FILENAME = "palavras.txt"
 class word(object):
 
      secretword=  []
-     
+     def test_triedletter(self,guessed):
+         game2.triedletter(guessed);
+         print game2.getletterguessed()
+     def test_isecretwordGuessed(self,letter):
+        game2=game()
+        game2.setletter("d")
+        if game2.getletter()=="d":
+            print "deu tudo certo no setletter"
+        self.setsecretword("aba") 
+        print self.getsecretword(); 
+        if  not self.isWordGuessed(game2):
+           game2.setletter("a")
+           guessed=''
+           game2.triedletter(guessed);
+           print game2.getletterguessed()
+           if not  self.isWordGuessed(game2):
+             game2.setletter("b")
+           self.isWordGuessed(game2)
+           if  self.isWordGuessed(game2):
+              print "deu tudo certo no metodo iswordguessed"  
+        
      def __init__ (self,secretword): 
          self.secretword=secretword
      
      def guessedword(self,game): 
-      letter=game.letter 
+      letter=game.getletter()
       guessed=''
       lettersGuessed=game.lettersGuessed
       for letter in self.secretword:
@@ -28,15 +48,17 @@ class word(object):
 
      def isWordGuessed(self, game):
         lettersGuessed=game.lettersGuessed
+        i=0
         for letter in self.getsecretword():
          if letter in lettersGuessed:
-            pass
+            i=i+1
+            print i
+            if i>=2:
+              print i
+              return True  
          else:
             return False
-
-         return True
   	 
-
      def quantitydifferent(self):
          secretword=self.getsecretword()
          quantityletters=len(secretword)   
@@ -64,12 +86,12 @@ class word(object):
          return numberofdifferent                  
              
             
-     def setsecretword(secretword):
-         self.secretword=secetword
+     def setsecretword(self,secretword):
+         self.secretword=secretword
      def getsecretword(self):
          return self.secretword     
                
-
+     
 
        
           
@@ -77,10 +99,53 @@ class game(object):
     guesses=8
     lettersGuessed =  []
     letter='y'
+    def test_score(self):
+        self.letter="c";
+        worda=word("aca")
+        guesses2=8;
+        self.score(worda)
+        if(self.guesses==guesses2):
+          print "deu tudo certo no score" 
+
+    def testsetletter(self):
+        print " e um boleano"
+        letter="a"
+        self.setletter(letter)
+        if letter==self.getletter():
+            print "o metodo funcionou"  
     def validaletra(self,letter):
-         if letter in string.ascii_lowercase:
-             return True
-         return 
+        if letter in string.ascii_lowercase:
+            return True
+        else:
+            
+            if letter.lower() in string.ascii_lowercase:
+                return True
+            else:
+                return False
+
+    def validaletraint(self,letter):
+            try:
+                letter.lower()
+                return self.validaletra(letter) 
+            except:
+                return False
+    def validapalavra(self,result):
+        for letter in result :
+            if self.validaletra(letter):
+               pass
+            else:
+               return False
+            print " e uma palavra"
+    def test_triedletter():
+        result=self.triedletter(guessed)    
+        if result or not result:
+            print "deu tudo certo"           
+    def test_getletterguessed(result):
+        
+        if letter==self.getletterguessed():
+            print " o metodo de tentativa funcionou" 
+        if result :
+            guessed=self.score(worda)                              
     def __init__(self):
         pass 
     def loadWords(self):
@@ -97,31 +162,38 @@ class game(object):
      wordlist = string.split(line)
      print "  ", len(wordlist), "words loaded."
      return random.choice(wordlist)
+    def validaint(self,integer):
+        if not self.validaletraint(integer): 
+            if integer>0:
+                return True 
+    def setletter(self,letter):
+           self.letter=letter             
     def gametry(self,secretWord) :
     
       print 'Welcome to the game, Hangam!'
       print 'I am thinking of a word that is', len(secretWord), ' letters long.'
       print '-------------'
       worda= word(secretWord)
-      worda.quantitydifferent()
+      i=worda.quantitydifferent()
+      self.validaint(i)
+      worda.test_isecretwordGuessed("a")
       guessed=' '    
       while worda.isWordGuessed(self) == False and self.guesses >0:
          print 'You have ', self.guesses, 'guesses left.'
          self.lettersavailable()
          invalid=True;
          while(invalid):
-           letter = raw_input('Please guess a letter: ')
-           invalid=not self.validaletra(letter)
-          
-           self.setletter(letter) 
-         result=self.triedletter(guessed)     
-         if result :
-            guessed=self.score(worda)    
+            letter = raw_input('Please guess a letter: ')
+            invalid=not self.validaletra(letter)
+         self.setletter(letter)
+         self.triedletter(guessed)
+         self.score(worda)
+         worda.guessedword(self)
       else:
            if worda.isWordGuessed(self) == True:
              print 'Congratulations, you won!'
            else:
-             print 'Sorry, you ran out of guesses. The word was ', worda.getsecretword, '.'
+             print 'Sorry, you ran out of guesses. The word was ', worda.getsecretword(), '.'
     def getAvailableLetters(self):
         import string
         # 'abcdefghijklmnopqrstuvwxyz'
@@ -145,15 +217,14 @@ class game(object):
     def score(self,worda):
          letter=self.letter          
          guessed=worda.guessedword(self)          
-         if self.getletter() in worda.getsecretword():
+         if letter in worda.getsecretword():
             print 'Good Guess: ', guessed
          else:
             self.guesses -=1 
             print 'Oops! That letter is not in my word: ',  guessed
          print '------------'
          return guessed
-    def setletter(self,letter):
-           self.letter=letter 
+   
     def getletter(self):
            return self.letter
     def getletterguessed(self):
@@ -162,4 +233,5 @@ class game(object):
       
 game1=game()
 secretWord = game1.loadWords().lower()
+game1.validapalavra(secretWord)
 game1.gametry(secretWord)
